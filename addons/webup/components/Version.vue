c3841e2edc3b4265f2970d45725ce7fcddce546a
Haili Zhang <haili.zhang@outlook.com>
2023-08-02 15:57:41 +0800

    ✨ feat(addons/webup): add `Version` component
    Signed-off-by: Haili Zhang <haili.zhang@outlook.com>

---
+++ addons/webup/components/Version.vue
@@ -0,0 +1,3 @@
+<template>
+  <span class="bg-$prism-background px-2 py-0 rounded font-mono inline-block leading-6 text-0.4em transform translate-y-[-1.5em] translate-x-[-0.3em] text-green-500"><slot /></span>
+</template>
